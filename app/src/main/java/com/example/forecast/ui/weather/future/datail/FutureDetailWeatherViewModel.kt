c8615c70a77d07457a99da5f61d340235b450958
Bohdan Sushchak <bogdansushak@gmail.com>
2018-12-08 19:59:15 +0000

    Created fragment and implemented navigation

---
+++ app/src/main/java/com/example/forecast/ui/weather/future/datail/FutureDetailWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.forecast.ui.weather.future.datail
+
+import androidx.lifecycle.ViewModel;
+
+class FutureDetailWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
